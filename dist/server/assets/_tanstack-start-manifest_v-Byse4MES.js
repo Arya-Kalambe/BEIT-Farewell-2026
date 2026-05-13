1db50f1b08fbd0f1c1171e9b6d7b5f58741dad29
Arya-Kalambe <aryakalambe23@gmail.com>
2026-05-14 01:19:48 +0530

    Delete dist directory

--- dist/server/assets/_tanstack-start-manifest_v-Byse4MES.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/Dell/Downloads/forever-farewell-main/forever-farewell-main/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-DDdlq9R_.js"] }, "/": { filePath: "C:/Users/Dell/Downloads/forever-farewell-main/forever-farewell-main/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-4VZDvJQq.js"] } }, clientEntry: "/assets/index-DDdlq9R_.js" });
-export {
-  tsrStartManifest
-};
--- dist/server/index.js
+++
@@ -1,5 +0,0 @@
-import { w } from "./assets/worker-entry-D-x42j5g.js";
-import "node:events";
-export {
-  w as default
-};
